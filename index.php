bfaa7378669703a8aa1e6f127ae85c0cf87daaf4
Subash <subash@subash>
2016-11-18 19:21:09 +0530

    structring code

--- index.php
+++ index.php
@@ -1,56 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require "vendor/autoload.php";
 
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-</head>
+$base_url = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$base_url = substr($base_url,strpos($base_url,'index.php'));
+$base_url = str_replace("index.php","",$base_url );
+echo $base_url;exit;
 
-<body>
-<br/><br/>
-  <div class="container">
-    <div class="panel panel-default">
-      <div class="panel-heading">Panel Heading</div>
-      <div class="panel-body">
-        <form>
-          <div class="form-group">
-            <label for="email">Firstname:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email">
-          </div>
-          <div class="form-group">
-            <label for="pwd">Lastname:</label>
-            <input type="text" class="form-control" id="pwd" placeholder="Enter password">
-          </div>
-
-          <div class="form-group">
-            <label for="pwd">Street:</label>
-            <input type="text" class="form-control" id="pwd" placeholder="Enter password">
-          </div>
-
-          <div class="form-group">
-            <label for="pwd">Zipcode:</label>
-            <input type="text" class="form-control" id="pwd" placeholder="Enter password">
-          </div>
-
-          <div class="form-group">
-            <label for="pwd">City:</label>
-            <input type="text" class="form-control" id="pwd" placeholder="Enter password">
-          </div>
-
-
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Remember me</label>
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</body>
-
-</html>
+?>
--- models/Cities.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace subash;
-
-class Cities extends Database
